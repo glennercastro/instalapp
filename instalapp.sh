3337a0f049dc29c0364496241933461f1cdc97b8
ENC SegTi <44949748+glennercastro@users.noreply.github.com>
2021-08-05 16:09:00 -0300

    Update instalapp.sh

--- instalapp.sh
+++ instalapp.sh
@@ -23,104 +23,132 @@ echo "  ░░                    ░░
 #Update Linux
 echo -e " \033[01;32mRealizando Update\033[01;37m"
 sudo apt update -y
-echo 
-echo 
-echo 
-echo 
-echo 
+echo " 
+
+
+
+" 
 echo -e " \033[01;33mFim do Update\033[01;37m"
 #Fim do Update
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
+echo "1
+2
+3
+4
+5"
 #######################################################
 #Upgrade Linux
 echo -e " \033[01;32mRealizando Upgrade\033[01;37m"
 sudo apt upgrade -y
-echo
-echo
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo -e " \033[01;33mFim do Upgrade\033[01;37m"
 #Fim do Upgrade
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
+echo "1
+2
+3
+4
+5"
 #######################################################
 #sudo dpkg --configure -a
 echo -e " \033[01;32mIstalando Pacotes \033[01;37m"
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Instalando Tor"
 sudo apt install tor torbrowser-launcher -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 #echo "Instalando Zenmap"
 #sudo apt install zenmap -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Instalando McEdit"
 sudo apt install mc -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Istalando Alien"
 sudo apt install alien -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Istalando PlayonLinux"
 sudo apt install playonlinux -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Instalando G-Edit"
 sudo apt install gedit -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Instalando Ncat"
 sudo apt install ncat -y
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Baixando e Instalando TeamViewer "
 sudo wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-echo
+echo " 
+
+" 
 echo "Iniciando instação"
 echo
 sudo dpkg -i teamviewer*.deb
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Baixando e Instalando Angry IP Scanner"
 sudo wget https://github.com/angryip/ipscan/releases/download/3.7.6/ipscan_3.7.6_amd64.deb
-echo
+echo " 
+
+" 
 echo "Iniciando instação"
 echo
 sudo dpkg -i ipscan*.deb
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo "Baixando e Instalando Zenmap"
 sudo wget https://nmap.org/dist/zenmap-7.91-1.noarch.rpm
 sudo alien zenmap-7.91-1.noarch.rpm 
-echo
+echo " 
+
+" 
 echo "Iniciando instação"
 echo
 sudo dpkg -i zenmap*.deb
-echo
-echo
-echo
-echo
+echo " 
+
+
+
+" 
 echo -e " \033[01;33mFim dos Pocedimento\033[01;37m"
-echo
-#fi
-#echo -e " \033[01;41mRealizando Update\033[01;37m"
+####
+#fim
+#echo -e " \033[01;41mTEXTO\033[01;37m"
